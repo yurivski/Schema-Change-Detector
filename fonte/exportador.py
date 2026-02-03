c213833a0438de87027df2148985392ceebdd58e
Yuri Pontes <230902962+yurivski@users.noreply.github.com>
2026-02-03 16:41:23 -0300

    Refatorei o exportador, corrigi um bug na linhas 52, fiz alguns testes com alterações diretas no banco de dados, resultados dos testes na pasta historico/

--- fonte/exportador.py
+++ fonte/exportador.py
@@ -1,7 +1,6 @@
 # Conectar no PostgreSQL e extrair as informações completas das tabelas
 
-import os 
-import pandas as pd 
+import os
 from sqlalchemy import create_engine, inspect
 from dotenv import load_dotenv
 from datetime import datetime
@@ -19,18 +18,22 @@ conn_str = f'postgresql://{user}:{password}@{host}/{database}'
 engine = create_engine(conn_str)
 inspector = inspect(engine)
 
-tabelas = ['categories', 'customers', 'departments', 'order_items', 'orders', 'products']
-dicionario = {}
 
-# Dados da tabela
-for dados in tabelas:
-    colunas = inspector.get_columns(dados)
-    pk_info = inspector.get_pk_constraint(dados)
-    fk_info = inspector.get_foreign_keys(dados)
-    indices = inspector.get_indexes(dados)
+# Transformando a execução em função reutilizavel para ser importada no arquivo comparador
+"""
+Para um projeto com mais de um arquivo, precisamos criá-los para que a execução do arquivo
+base se transforme em uma função reutilizavel para usar em outro arquivo, 
+caso contrário, o trabalho será grande para refazer.
+"""
+def extrair_metadados(inspector, nome_tabela):
+    # dados da tabela
+    colunas = inspector.get_columns(nome_tabela)
+    pk_info = inspector.get_pk_constraint(nome_tabela)
+    fk_info = inspector.get_foreign_keys(nome_tabela)
+    indices = inspector.get_indexes(nome_tabela)
+    unique_constraints = inspector.get_unique_constraints(nome_tabela)
+    check_constraints = inspector.get_check_constraints(nome_tabela)
 
-    unique_constraints = inspector.get_unique_constraints(dados)
-    check_constraints = inspector.get_check_constraints(dados)
 
     info_colunas = {}
     for col in colunas:
@@ -46,34 +49,40 @@ for dados in tabelas:
             'primary_key': nome_col in pk_info.get('constrained_columns', []),
             'unique': any(nome_col in u['column_names'] for u in unique_constraints),
             #'tamanho_max': getattr(col['type'], 'length', None),
-            'foreign_key': fk_info
+            'foreign_key': [fk for fk in fk_info if nome_col in fk['constrained_columns']]
         }
 
 
-    dicionario[dados] = {
+    return {
         'capturado_em': datetime.now().isoformat(),
-        'nome': dados,
+        'tabela': nome_tabela,
         'qtd_colunas': len(colunas),
         'colunas': info_colunas,
         'indices': [idx['name'] for idx in indices],
-        'constraints_check': [c['sqlcheck'] for c in check_constraints]
+        'constraints_check': [[c['sqltext'] for c in check_constraints]]
+
     }    
 
-# Salvando a lista nos arquivos json nomeados de acordo com as tabelas:
-salvar_json = 'historico'
 print("\nSalvando dados na pasta")
 print()
 
-# Iterando pelo dicionário:
-for dados, info_colunas in dicionario.items():
-    timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    caminho_arquivo = os.path.join(salvar_json, f"{dados}_{timestamp}.json")
+if __name__ == "__main__":
+    tabelas = ['categories', 'customers', 'departments', 'order_items', 'orders', 'products', 
+                'order_detail_v', 'clientes_teste']
+
+# Salvando a lista nos arquivos json nomeados de acordo com as tabelas:
+salvar_json = 'historico'
+
+for metadados in tabelas:
+    extrair = extrair_metadados(inspector, metadados)
+    caminho_arquivo = os.path.join(salvar_json, f"{metadados}_DEPOIS_5x.json")
 
     try:
         with open(caminho_arquivo, 'w', encoding='utf-8') as f:
             # indent=4 deixa o arquivo organizado para leitura humana
             # # ensure_ascii=False permite caracteres especiais (acentos)
-            json.dump(info_colunas, f, indent=4, ensure_ascii=False)
+            json.dump(extrair, f, indent=4, ensure_ascii=False)
         print(f"Lista de dicionários salvos em: {caminho_arquivo}")
     except Exception as e:
-        print(f"Erro nos dicionários: {dados}: {e}")
+        print(f"Erro no dicionário: {metadados}: {e}")
+
