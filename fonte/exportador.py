77e15b8d2b8df1cbe1d15498bf1f245f8fd5026f
Yuri Pontes <230902962+yurivski@users.noreply.github.com>
2026-02-04 17:00:39 -0300

    Bugs na função salvar_json após refatoração. Corrigi: identação do json.dump dentro do with, ajustei parâmetro para receber lista de tabelas e adicionei criação de diretórios antes de salvar

--- fonte/exportador.py
+++ fonte/exportador.py
@@ -19,9 +19,12 @@ engine = create_engine(conn_str)
 inspector = inspect(engine)
 
 
-# Transformando a execução em função reutilizavel para ser importada no arquivo comparador
 def extrair_metadados(inspector, nome_tabela):
-    # dados da tabela
+    """
+    Extrai os metadados das colunas das tabelas do banco de dados PostgreSQL.
+    Metadados extraidos: colunas, PKs, FKs, Índices e Constraints
+    """
+
     colunas = inspector.get_columns(nome_tabela)
     pk_info = inspector.get_pk_constraint(nome_tabela)
     fk_info = inspector.get_foreign_keys(nome_tabela)
@@ -29,13 +32,12 @@ def extrair_metadados(inspector, nome_tabela):
     unique_constraints = inspector.get_unique_constraints(nome_tabela)
     check_constraints = inspector.get_check_constraints(nome_tabela)
 
-
+    # Mapeia as colunas
     info_colunas = {}
     for col in colunas:
         nome_col = col['name']
 
-        # Mapeamento detalhado das colunas:
-        # .__visit_name__ para extrair o tipo da coluna, ex: VARCHAR 
+        # Mapeamento detalhado das colunas: 
         # 'tipo': str(col['type']) para extrair o tipo completo, ex: VARCHAR(50)
         info_colunas[nome_col] = {
             'tipo': str(col['type']),
@@ -43,7 +45,7 @@ def extrair_metadados(inspector, nome_tabela):
             'default': col.get('default'),
             'primary_key': nome_col in pk_info.get('constrained_columns', []),
             'unique': any(nome_col in u['column_names'] for u in unique_constraints),
-            #'tamanho_max': getattr(col['type'], 'length', None),
+            # Usei list comprehension para filtrar FKs especídifcas desta coluna
             'foreign_key': [fk for fk in fk_info if nome_col in fk['constrained_columns']]
         }
 
@@ -54,30 +56,43 @@ def extrair_metadados(inspector, nome_tabela):
         'qtd_colunas': len(colunas),
         'colunas': info_colunas,
         'indices': [idx['name'] for idx in indices],
-        'constraints_check': [[c['sqltext'] for c in check_constraints]]
+        'constraints_check': [c['sqltext'] for c in check_constraints]
 
     }    
 
-print("\nSalvando dados na pasta")
-print()
+def salvar_json(tabelas, diretorio='historico', sufixo=None):
 
+    if sufixo is None:
+        sufixo = datetime.now.strftime('%y-%m-%d_%H-%M-%S')
+
+    print(f"\nJson salvo em: {diretorio}/")
+
+    for tabela in tabelas:
+        
+        try:
+            schema = extrair_metadados(inspector, tabela)
+            arquivo = f"{tabela}_{sufixo}.json"
+            caminho_arquivo = os.path.join(diretorio, arquivo)
+            with open(caminho_arquivo, 'w', encoding='utf-8') as f:
+                # indent=4 deixa o arquivo organizado para leitura humana
+                # # ensure_ascii=False permite caracteres especiais (acentos)
+                json.dump(schema, f, indent=4, ensure_ascii=False)
+            print(f"{tabela}: {arquivo}")
+        except Exception as e:
+                print(f"Erro no dicionário: {tabela}: {e}")
+
+# Execução principal
 if __name__ == "__main__":
-    tabelas = ['categories', 'customers', 'departments', 'order_items', 'orders', 'products', 
-                'order_detail_v', 'clientes_teste']
 
-# Salvando a lista nos arquivos json nomeados de acordo com as tabelas:
-salvar_json = 'historico'
-
-for metadados in tabelas:
-    extrair = extrair_metadados(inspector, metadados)
-    caminho_arquivo = os.path.join(salvar_json, f"{metadados}_DEPOIS_5x.json")
-
-    try:
-        with open(caminho_arquivo, 'w', encoding='utf-8') as f:
-            # indent=4 deixa o arquivo organizado para leitura humana
-            # # ensure_ascii=False permite caracteres especiais (acentos)
-            json.dump(extrair, f, indent=4, ensure_ascii=False)
-        print(f"Lista de dicionários salvos em: {caminho_arquivo}")
-    except Exception as e:
-        print(f"Erro no dicionário: {metadados}: {e}")
+    tabelas = ['categories', 
+                'customers', 
+                'departments', 
+                'order_items', 
+                'orders', 
+                'products', 
+                'order_detail_v', 
+                'clientes_teste']
+    
+    # Salvando a lista em arquivo json noameado de acordo com a tabelas
+    salvar_json(tabelas, diretorio='historico', sufixo=datetime.now())
 
